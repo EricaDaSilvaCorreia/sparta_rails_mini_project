dcaa2c3d5d443213ae381010aaddd674dfcc7f69
Tech-A07 <rlecoutre@spartaglobal.com>
2018-10-05 12:02:50 +0100

    added landing page and bootstrap gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_104811) do
+ActiveRecord::Schema.define(version: 2018_10_05_105857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "statics", force: :cascade do |t|
+    t.string "landing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teches", force: :cascade do |t|
+    t.string "technology"
+    t.date "date_created"
+    t.integer "version"
+    t.text "desc"
+    t.string "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
